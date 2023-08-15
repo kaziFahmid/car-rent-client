7bf79d2cd6e4a2fa1b04c62b1e3702768a3affe8
unknown <nirjhorislam42@gmail.com>
2023-08-16 03:53:57 +0600

    firbase updated

--- firebase.config.js
+++ firebase.config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,10 +6,10 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
-  projectId:  import.meta.env.VITE_projectId,
+  projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId:  import.meta.env.VITE_appId
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
